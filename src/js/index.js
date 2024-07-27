47511a67bfca22d56ead249616af1cc25c9fd16f
WRyan <wesley.ryan03@gmail.com>
2024-07-27 20:31:50 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,33 @@
+const btnAvancar = document.getElementById("btn-avancar");
+const btnVoltar = document.getElementById("btn-voltar");
+let cartaoAtual = 0;
+const cartoes = document.querySelectorAll(".cartao");
+
+btnAvancar.addEventListener("click", function () {
+    esconderCartaoSelecionado();
+
+    if (cartaoAtual === cartoes.length - 1) {
+        cartaoAtual = 0; // Volta para o primeiro cartão
+    } else {
+        cartaoAtual++;
+    }
+
+    mostrarCartao();
+});
+
+btnVoltar.addEventListener("click", function () {
+    if (cartaoAtual === 0) return;
+
+    esconderCartaoSelecionado();
+    cartaoAtual--;
+    mostrarCartao();
+});
+
+function mostrarCartao() {
+    cartoes[cartaoAtual].classList.add("selecionado");
+}
+
+function esconderCartaoSelecionado() {
+    const cartaoSelecionado = document.querySelector(".selecionado");
+    cartaoSelecionado.classList.remove("selecionado");
+}
